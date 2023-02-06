8c01564768a2e67ff3d791a0c42b3393a4382c22
malikovaasem <82565769+malikovaasem@users.noreply.github.com>
2023-02-06 23:14:00 +0900

    second commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,6 +4,24 @@ import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
+        UserServiceImpl us = new UserServiceImpl();
+
+        us.dropUsersTable();
+        us.createUsersTable();
+        us.saveUser("'John'", "'Johnson'", (byte) 38);
+        us.saveUser("'Mickael'", "'Jackson'", (byte) 23);
+        us.saveUser("'Robert'", "'Daune'", (byte) 25);
+        us.saveUser("'Withney'", "'Huston'", (byte) 41);
+
+        List<User> usersList = us.getAllUsers();
+
+        for (User userr : usersList) {
+            System.out.println(userr.toString());
+        }
+
+        us.cleanUsersTable();
+
+        us.removeUserById(3);
 
     }
 }
